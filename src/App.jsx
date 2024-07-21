b4f29cb929e4a0d05057ed540905cc298ca29976
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-07-21 00:51:13 -0300

    Revert "feat: ends the company introduction section"
    This reverts commit e790b9376a2fc81cbd3033a11719f3174bbd6d68.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './components/Header';
+import Introduction from './components/Introduction';
 
 function App() {
 
     return (
         <>
             <Header />
+            <main className='max-w-1216 mx-auto'>
+                <Introduction />
+            </main>
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,18 @@ export default {
             colors: {
                 'brown-primary': '#3C0E0E',
                 'rose-primay': '#EB8E8E'
+            },
+            margin: {
+                '[-40]': '-40px'
+            },
+            spacing: {
+                '1216': '1216px',
+                '550': '550px',
+                '485': '485px',
+                '[-260]': '-260px',
+                '[-50]': '-50px',
             }
         },
     },
     plugins: [],
 }
-
