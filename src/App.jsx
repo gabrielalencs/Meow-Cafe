e790b9376a2fc81cbd3033a11719f3174bbd6d68
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-07-21 00:50:37 -0300

    feat: ends the company introduction section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css';
 import Header from './components/Header';
-import Introduction from './components/Introduction';
 
 function App() {
 
     return (
         <>
             <Header />
-            <main className='max-w-1216 mx-auto'>
-                <Introduction />
-            </main>
         </>
     )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -27,15 +27,15 @@ const Header = () => {
 
 
     return (
-        <header className='max-w-1216 mx-auto pt-5 z-10 relative px-10 flex items-center justify-between'>
+        <header className='pt-5 px-10 flex items-center justify-between relative right-6'>
             <div className='flex items-center'>
-                <img src={headerLogo} alt="logo da empresa meow café" className='relative right-10' />
-                <span className='hidden m-[-40] text-xl font-bold sm:inline'>Meow Café</span>
+                <img src={headerLogo} alt="logo da empresa meow café" />
+                <span className='hidden text-xl font-bold sm:inline'>Meow Café</span>
             </div>
 
             <div className='inline-block lg:hidden'>
                 <label className="hamburger" >
-                    <input type="checkbox" checked={menuVisible} onChange={handleMenuMobileClick} />
+                    <input type="checkbox" checked={menuVisible} onClick={handleMenuMobileClick} />
 
                     <svg viewBox="0 0 32 32">
                         <path className="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
@@ -46,9 +46,9 @@ const Header = () => {
 
             <nav className={`absolute right-10 top-36 bg-rose-primay rounded-b-xl duration-500 ${menuVisible ? 'showMenuMobile' : 'hiddenMenuMobile'} lg:bg-transparent lg:duration-0 lg:h-max lg:static lg:flex lg:items-center lg:gap-20`}>
                 <ul className='flex flex-col gap-4 text-center lg:flex-row lg:gap-14'>
-                    <li className='navLink navLinkAfter hover:after:w-full'>Início</li>
-                    <li className='navLink navLinkAfter hover:after:w-full'>Produtos</li>
-                    <li className='navLink navLinkAfter hover:after:w-full'>Sobre</li>
+                    <li className='navLink'>Início</li>
+                    <li className='navLink'>Produtos</li>
+                    <li className='navLink'>Sobre</li>
                 </ul>
 
                 <Button text='Delivery' classe='btnHeader' />
