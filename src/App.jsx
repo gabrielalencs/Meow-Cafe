c63676ff4ed159bbd42959f86264c67083cb926b
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-07-21 23:04:28 -0300

    refactor: change the name of the intro component folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/Header';
-import Introduction from './components/Introduction';
+import Introduction from './components/IntroductionSection';
 
 function App() {
 
